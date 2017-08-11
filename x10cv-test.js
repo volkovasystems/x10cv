1d249ef866e9f36886be56879b7de3a3557d074b
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-11 13:55:49 +0800

    Updated test.

--- x10cv-test.js
+++ x10cv-test.js
@@ -2,11 +2,8 @@ const assert = require( "assert" );
 const x10cv = require( "./x10cv.js" );
 
 let Hello = function Hello( ){ };
-
 Hello[ Symbol.for( "extensive" ) ] = Symbol.for( "extensive" );
 
-/* These result of these test changed from true to false
 assert.equal( x10cv( Hello ), true, "should be equal to true" );
-*/
 
 console.log( "ok" );
